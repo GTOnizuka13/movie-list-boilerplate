85ab8d1a72632b777853d2cb65cd7e29b97d1f88
GTOnizuka13 <darkrain0123@gmail.com>
2021-03-25 23:35:22 -0700

    Search bar component added but does not return correctly debug!

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,7 +5,8 @@ import SearchMovie from './SearchMovie.jsx'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {list:
+    this.state = {
+      list:
       [
         { title: 'Mean Girls' },
         { title: 'Hackers' },
@@ -13,12 +14,23 @@ class App extends React.Component {
         { title: 'Sunshine' },
         { title: 'Ex Machina' }
       ]
-    }
+    };
+    this.onSearch = this.onSearch.bind(this);
   }
+// fix always returns first statement no matter what
+  onSearch(searched) {
+    this.state.list.map((item) => (console.log(item.title.includes(searched))))
+    if(this.state.list.map((item) => (item.title.includes(searched)))) {
+      return `Sorry  is unavailable`;
+    }
+    return `Yes we do have !`;
+  }
+
   render() {
     return (
       <div>
         <h1>Movie List</h1>
+        <SearchMovie movieList={this.state.list} onSearch={this.onSearch}/>
         <MovieList movieList={this.state.list} />
       </div>
     )
--- client/src/components/SearchMovie.jsx
+++ client/src/components/SearchMovie.jsx
@@ -5,11 +5,34 @@ import MovieList from './MovieList.jsx'
 class SearchMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.state;
+    this.state = {
+      title:''
+    };
+    // bind 'this' to the constructor function
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({title: event.target.value})
   }
 
-  render() {
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(this.props.onSearch(this.state.title));
+   // alert('Looking for ' + this.state.title);
+  }
 
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Looking for Something?
+          <input type="text" value={this.state.title} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Go!"/>
+      </form>
+    );
   }
 };
 
